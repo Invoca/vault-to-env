2fee8da13b654f3727d850fc950102c1f3517353
Jesse Adametz <jadametz@invoca.com>
2017-12-07 15:12:28 -0800

    Initial app and tests

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type arrayFlag []string
+
+func (a *arrayFlag) Set(value string) error {
+	*a = append(*a, strings.TrimSpace(value))
+	return nil
+}
+
+func (a *arrayFlag) String() string {
+	return fmt.Sprint([]string(*a))
+}
+
+func setEnvs(envVars, values []string) {
+	for i, envVar := range envVars {
+		os.Setenv(envVar, values[i])
+	}
+}
+
+type vaultResponse struct {
+	RequestID     string                 `json:"request_id"`
+	LeaseID       string                 `json:"lease_id"`
+	LeaseDuration int                    `json:"lease_duration"`
+	Data          map[string]interface{} `json:"data"`
+	Warnings      []string               `json:"warnings"`
+	WrapInfo      string                 `json:"wrap_info,omitempty"`
+	Auth          string                 `json:"auth,omitempty"`
+}
+
+func queryVault(url, token string, paths []string) []string {
+	var client http.Client
+	var values []string
+
+	for _, path := range paths {
+		vr := vaultResponse{}
+
+		reqURL := url + "/v1/" + path
+		req, err := http.NewRequest("GET", reqURL, nil)
+		if err != nil {
+			fmt.Printf("unable to create request; %v", err)
+		}
+		req.Header.Set("X-Vault-Token", token)
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("unable to get response; %v", err)
+		}
+		defer resp.Body.Close()
+
+		if err := json.NewDecoder(resp.Body).Decode(&vr); err != nil {
+			fmt.Printf("unable to decode response body; %v", err)
+		}
+
+		vrData, err := json.Marshal(&vr.Data)
+		if err != nil {
+			fmt.Printf("unable to marshall vrData; %v", err)
+		}
+		values = append(values, string(vrData))
+	}
+
+	return values
+}
+
+func main() {
+	var paths, envVars arrayFlag
+
+	url := flag.String("url", "", "The Vault URL to query.")
+	token := flag.String("token", "", "The token to query Vault with.")
+	flag.Var(&paths, "path", "Path to secret being queried. Can be provided multiple times.")
+	flag.Var(&envVars, "evar", "Env variable to store secret in. Can be provided multiple times.")
+	flag.Parse()
+
+	results := queryVault(*url, *token, paths)
+	setEnvs(envVars, results)
+}
---
+++ main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	. "flag"
+	"net/http"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+)
+
+func TestArrayFlagSet(t *testing.T) {
+	var flags FlagSet
+	flags.Init("test", ContinueOnError)
+
+	var a arrayFlag
+	flags.Var(&a, "a", "usage")
+	if err := flags.Parse([]string{"-a", "foo", "-a", "bar", "-a=foobar"}); err != nil {
+		t.Error(err)
+	}
+	if len(a) != 3 {
+		t.Fatal("expected 3 args; got ", len(a))
+	}
+	expect := "[foo bar foobar]"
+	if a.String() != expect {
+		t.Errorf("expected value %q got %q", expect, a.String())
+	}
+}
+
+func TestSetEnvs(t *testing.T) {
+	envVars := []string{"foo", "bar"}
+	values := []string{"biz", "baz"}
+
+	setEnvs(envVars, values)
+
+	for i, envVar := range envVars {
+		if result := os.Getenv(envVar); values[i] != result {
+			t.Errorf("expected %v; got %v", values[i], result)
+		}
+	}
+}
+
+func TestQueryVault(t *testing.T) {
+	url := "http://localhost:8200"
+	token := "roottoken"
+	paths := []string{"secret/password"}
+	values := []string{`{"value":"itsasecret"}`}
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", "http://localhost:8200/v1/secret/password", httpmock.NewStringResponder(http.StatusOK,
+		`{"request_id":"","lease_id":"","renewable":false,"lease_duration":2764800,"data":{"value":"itsasecret"},"wrap_info":null,"warnings":null,"auth":null}`))
+
+	results := queryVault(url, token, paths)
+
+	if !reflect.DeepEqual(results, values) {
+		t.Errorf("expected %v; got %v", results, values)
+	}
+}
